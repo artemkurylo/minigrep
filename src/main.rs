546862d15851a405bffef49505951bc98518f77d
kuryloartem <artem.kurylo@rndpoint.com>
2023-07-26 15:48:02 +0200

    minigrep version 1.0 - Added print to err output instead of standard output

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ fn main() {
 
     let config = Config::build(&args)
         .unwrap_or_else(|err| {
-            println!("Problem parsing arguments: {}", err);
+            eprintln!("Problem parsing arguments: {}", err);
 
             process::exit(1);
         });
 
     if let Err(err) = minigrep::run(config) {
-        println!("Application error: {}", err);
+        eprintln!("Application error: {}", err);
 
         process::exit(1);
     }
